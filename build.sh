8761404d4d45d88fcce7bdb932fc994cafd769d0
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-06-24 19:45:30 +0200

    Added Source Link support

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Add the dotnet path to the path
+export PATH="$HOME/.dotnet":$PATH
+
+if [ -d "./artifacts" ]
+then
+    rm -Rf "./artifacts"; 
+fi
+
+dotnet restore
+
+commitHash=$(git rev-parse --short HEAD)
+suffix="-ci-local"
+buildSuffix="$suffix-$commitHash"
+
+echo "build: Version suffix is $buildSuffix"
+
+dotnet build -c Release --version-suffix "$buildSuffix"  -v q /nologo
+
+echo "Running unit tests"
+dotnet test ./tests/UnitTests/UnitTests.csproj
+
+
+dotnet pack ./src/AwesomeLibrary/AwesomeLibrary.csproj -c Release -o ./artifacts --include-symbols --no-build --version-suffix=$buildSuffix
